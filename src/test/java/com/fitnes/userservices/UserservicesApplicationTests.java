a818c4f47ffc9126e29371eb818fdaee4efb606c
Azharuddin Ali <azharuddinali017@gmail.com>
2025-10-19 16:40:31 +0530

    bolier code added

---
+++ src/test/java/com/fitnes/userservices/UserservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitnes.userservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
